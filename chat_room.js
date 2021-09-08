9c7db61ccebcf4feaa0fb6aa2f8fe6c39601d6b9
Writaja <74670011+Writaja@users.noreply.github.com>
2021-09-08 22:21:35 +0530

    Add files via upload

--- chat_room.js
+++ chat_room.js
@@ -30,5 +30,52 @@ getData();
 function redirecttoroomname(name){
       console.log(name);
       localStorage.setItem("room_name",name);
-      window.location="kwitter_page.html";
+      window.location="chat_page.html";
 }
+firebase.initializeApp(firebaseConfig);
+    user_name=localStorage.getItem("user_name");
+    room_name=localStorage.getItem("room_name");
+    function send(){
+          msg=document.getElementById("msg").value;
+firebase.database().ref(room_name).push({
+      uname:user_name,
+      message:msg,
+      like:0
+});
+document.getElementById("msg").value="";
+
+    }
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+//Start code
+console.log(firebase_message_id);
+console.log(message_data);
+uname=message_data["uname"];
+message=message_data["message"];
+like=message_data["like"];
+name_with_tag="<h4> "+uname+"<img class='user_tick' sre='3da3e019767446593d6bec8547f57b5d.jpg.png'></h4>";
+message_tag="<h4 class='message_h4'>"+message+"</h4>";
+like_button="<button class='btn btn-warning' id="+firebase_message_id+" value"+like+" onclick='updatelike(this.id)'>";
+span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>Like: "+like+"</span> </button> <hr>";
+row=name_with_tag+message_tag+like_button+span_with_tag;
+document.getElementById("output").innerHTML+=row;
+ 
+//End code
+      } });  }); }
+
+getData();
+function updatelike(message_id){
+      button_id=message_id;
+      noOfLikes=document.getElementById(button_id).value;
+      updatedlike=Number(noOfLikes)+1;
+      firebase.database().ref(room_name).child(message_id).update({
+            like:updatedslike
+      });
+       }
+function logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="index.html";
+}
+
